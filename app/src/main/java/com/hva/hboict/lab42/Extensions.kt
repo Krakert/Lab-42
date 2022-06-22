f0c69c883f25eac57aa67c70d81863d50ad6ee7e
Davy van der Meijden <davy.van.der.meijden@hva,nl>
2022-06-22 18:05:25 +0200

    Development of UVA application (Speach, welcomeing and targeting)

--- app/src/main/java/com/hva/hboict/lab42/Extensions.kt
+++ app/src/main/java/com/hva/hboict/lab42/Extensions.kt
@@ -5,11 +5,21 @@ import android.util.Log
 import com.aldebaran.qi.Consumer
 import com.aldebaran.qi.Future
 import com.aldebaran.qi.sdk.QiContext
+import com.aldebaran.qi.sdk.`object`.conversation.Phrase
+import com.aldebaran.qi.sdk.`object`.conversation.Say
 import com.aldebaran.qi.sdk.`object`.human.*
 import com.aldebaran.qi.sdk.`object`.humanawareness.HumanAwareness
+import com.aldebaran.qi.sdk.`object`.locale.Language
+import com.aldebaran.qi.sdk.`object`.locale.Locale
+import com.aldebaran.qi.sdk.`object`.locale.Region
 import com.aldebaran.qi.sdk.builder.ApproachHumanBuilder
 import com.aldebaran.qi.sdk.builder.EngageHumanBuilder
+import com.aldebaran.qi.sdk.builder.SayBuilder
+import kotlinx.coroutines.delay
+import java.time.Instant
+import java.time.format.DateTimeFormatter
 import java.util.*
+import kotlin.concurrent.schedule
 
 class HumanEngager(
     private val qiContext: QiContext, // Inner state, from which state is calculated
@@ -22,6 +32,9 @@ class HumanEngager(
     private var queuedRecommendedHuman: Human? = null
     private var disengageTimerTask: TimerTask? = null
     var onInteracting: Consumer<Human>? = null
+    var currentTimestamp = System.currentTimeMillis()
+    var trigger = true
+
 
     /* Internal; notify listener of "isInteracting" state.
 	 */
@@ -42,13 +55,14 @@ class HumanEngager(
             engaging = true
 
             // Get the characteristics, and log them.
-            Log.i(TAG, "Age: ${human.estimatedAge.years} year(s)")
+            Log.i(TAG, "${human.estimatedAge.years}")
             Log.i(TAG, "Gender: ${human.estimatedGender}")
             Log.i(TAG, "Pleasure state: ${human.emotion.pleasure}")
             Log.i(TAG, "Excitement state: ${human.emotion.excitement}")
             Log.i(TAG, "Engagement state: ${human.engagementIntention}")
             Log.i(TAG, "Smile state: ${human.facialExpressions.smile}")
             Log.i(TAG, "Attention state: ${human.attention}")
+
 
             val engage = EngageHumanBuilder.with(qiContext).withHuman(human).build()
             engage.addOnHumanIsEngagedListener {
@@ -90,26 +104,58 @@ class HumanEngager(
             tryToEngageHuman(human)
         }
 
-        awareness.async().addOnRecommendedHumanToApproachChangedListener { human: Human ->
+        awareness.async().addOnRecommendedHumanToApproachChangedListener { human: Human? ->
             // Build the action.
             // Robot will approach human (ride to him)
-            val approachHuman = ApproachHumanBuilder.with(qiContext)
-                .withHuman(human)
+
+            val helloPhrases = listOf(
+                "Hallo daar!",
+                "Hey!", "Hallo!", "Hoi!",
+                "Goeiedag!", "Ewa"
+            )
+            val randomElement = helloPhrases.random()
+
+            val phrase = Phrase(randomElement)
+            println(currentTimestamp)
+            val locale = Locale(Language.DUTCH, Region.NETHERLANDS)
+
+            val say: Say = SayBuilder.with(qiContext)
+                .withPhrase(phrase)
+                .withLocale(locale)
                 .build()
 
-            // Run the action asynchronously.
-            approachHuman.async().run()
+            if (human != null) {
+                val approachHuman = ApproachHumanBuilder.with(qiContext)
+                    .withHuman(human)
+                    .build()
+                // Run the action asynchronously.
+                approachHuman.async().run()
+            }
+
+
+
+
+            if (trigger) {
+                say.async().run()
+                trigger = false
+                currentTimestamp = System.currentTimeMillis()
+            }
+            if (System.currentTimeMillis() - currentTimestamp > 20000) {
+                trigger = true
+            }
         }
+
+        /* Start tracking and engaging humans.
+         */
+        fun stop() {
+            awareness.removeAllOnRecommendedHumanToEngageChangedListeners()
+            if (disengageTimerTask != null) {
+                disengageTimerTask!!.cancel()
+            }
+        } // Internal API
+
     }
 
-    /* Start tracking and engaging humans.
-	 */
-    fun stop() {
-        awareness.removeAllOnRecommendedHumanToEngageChangedListeners()
-        if (disengageTimerTask != null) {
-            disengageTimerTask!!.cancel()
-        }
-    } // Internal API
 
 }
 
--- app/src/main/java/com/hva/hboict/lab42/MainActivity.kt
+++ app/src/main/java/com/hva/hboict/lab42/MainActivity.kt
@@ -13,13 +13,11 @@ import com.aldebaran.qi.sdk.`object`.conversation.Say
 import com.aldebaran.qi.sdk.`object`.locale.Language
 import com.aldebaran.qi.sdk.`object`.locale.Locale
 import com.aldebaran.qi.sdk.`object`.locale.Region
-import com.aldebaran.qi.sdk.builder.ApproachHumanBuilder
 import com.aldebaran.qi.sdk.builder.SayBuilder
 import com.aldebaran.qi.sdk.design.activity.RobotActivity
 import com.hva.hboict.lab42.databinding.ActivityMainBinding
 
 class MainActivity : RobotActivity(), RobotLifecycleCallbacks {
-
     // The QiContext provided by the QiSDK.
     private var qiContext: QiContext? = null
     private var humanEngager: HumanEngager? = null
@@ -45,31 +43,65 @@ class MainActivity : RobotActivity(), RobotLifecycleCallbacks {
 
     }
 
+    private fun speach(phrase: String, pitch: Int, speed: Int) {
+        val locale = Locale(Language.DUTCH, Region.NETHERLANDS)
+        val pitchS = "\\vct=$pitch\\"
+        val speedS = "\\rspd=$speed\\"
+        val phraseBuild = Phrase("$speedS  $pitchS  $phrase")
+        println(phraseBuild)
+        val say: Say = SayBuilder.with(qiContext)
+            .withPhrase(phraseBuild)
+            .withLocale(locale)
+            .build()
+
+        return say.run()
+    }
+
+    private fun ageSpeach(age: Int): Boolean {
+        return age > 18
+    }
+
     override fun onRobotFocusGained(qiContext: QiContext) {
         this.qiContext = qiContext
         humanEngager = HumanEngager(this.qiContext!!, 100)
         humanEngager?.onInteracting = Consumer {
 
+
             if (it != null) {
                 // Create a phrase.
-                val locale: Locale = Locale(Language.DUTCH, Region.NETHERLANDS)
-                val phrase: Phrase = Phrase("Je ziet eruit als een ${it.estimatedGender}")
+                val estemAge = it.estimatedAge.toString()
+                val result = estemAge.filter { it.isDigit() }
+                val age = Integer.parseInt(result)
 
-                // Build the action.
-                val say: Say = SayBuilder.with(qiContext)
-                    .withPhrase(phrase)
-                    .withLocale(locale)
-                    .build()
+                println(ageSpeach(age))
+                val lingo = "\\vct=20\\\\rspd=50\\G-R-A-T-I-S   \\pau=750\\    \\rspd=70\\gratis"
 
-                // Run the action synchronously.
-                say.run()
 
-                val approachHuman = ApproachHumanBuilder.with(qiContext)
-                    .withHuman(it)
-                    .build()
+                val phrasesOld = listOf(
+                    "U ziet er goed uit vandaag",
+                    "Welkom in dit mooie pand, leuk dat u er bent",
+                    "Ik heet u welkom op de U-FAA",
+                    "De U-FAA blijft zich uitbreiden",
+                    "De techniek hier staat nooit stil",
+                    "Dit gebouw is echt mooi modern",
+                    "Wist u dat het getal 42 veelvoorkomend is in de wetenschap, het zou het ultieme antwoord op het leven zijn."
+                )
+                val phrasesYoung = listOf(
+                    "Er zitten hier veel pokemons",
+                    "Hey jij ziet er mooi uit!", "Welkom op de U-FAA", "Wat leuk dat je hier bent",
+                    "Hoe noem je een oude sneeuwpop? ........... Water!"
+                )
 
-                // Run the action asynchronously.
-                approachHuman.async().run()
+                if (age > 0) {
+                    if (ageSpeach(age)) {
+                        val randomElement = phrasesOld.random()
+                        speach(randomElement, 100, 80)
+                    } else {
+                        val randomElement = phrasesYoung.random()
+                        speach((randomElement), 100, 80)
+                    }
+
+                }
             }
 
         }
@@ -81,3 +113,4 @@ class MainActivity : RobotActivity(), RobotLifecycleCallbacks {
     override fun onRobotFocusRefused(reason: String?) {}
 
 }
+
